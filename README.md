c1aa060fec25427d7824266d96f28d2217a4d9fb
SantiagoTonarelli <santitona98@gmail.com>
2021-06-15 14:10:59 -0300

    fin

--- README.md
+++ README.md
@@ -1,7 +1,23 @@
+# GUIE    
+
+![](text.gif)
+
+## Es necesario tener instalado nodejs, luego nos paramos en el proyecto y corremos el siguiente comando:
+
 ````````````````
-GUIE    
+npm i    
 ````````````````
 
+## Luego ejecutar el siguiente comando: 
 
+````````````````
+node index -z (ACA VA LA DIRECCION DEL ZIP SIN PARENTESIS) -p (ACA VA LA DIRECCION DEL PROYECTO SIN PARENTESIS) -f (ACA VA EL NOMBRE DEL ARCHIVO SIN PARENTESIS)
+````````````````
 
-npm i
+### Ejemplo: 
+````````````````
+node index -z /Users/patoloco/Desktop/assets_Registration_addCard_2021-06-15.zip -p /Users/patoloco/Desktop/whitelabel-android-taco-time-nw -f milanesa
+````````````````
+
+# Que lo difruten!
+
--- index.js
+++ index.js
@@ -1,5 +1,32 @@
 const fs = require("fs");
+const figlet = require("figlet");
 const unzip = require("unzipper");
+const argv = require("yargs")
+	.option("z", {
+		alias: "zip",
+		type: "string",
+		demandOption: true,
+	})
+	.option("f", {
+		alias: "file",
+		type: "string",
+		demandOption: true,
+	})
+	.option("p", {
+		alias: "project",
+		type: "string",
+		demandOption: true,
+	})
+	.check((argv, options) => {
+		if (argv.z === null || argv.z === "") {
+			throw "Es necesario pasar la direccion del zip, NABO";
+		} else if (argv.p === null || argv.p === "") {
+			throw "Es necesario pasar la direccion del proyecto, GIL";
+		} else if (argv.f === null || argv.f === "") {
+			throw "Es necesario pasar el nombre del archivo Maestro";
+		}
+		return true;
+	}).argv;
 
 const drawableFolder = [
 	"drawable-hdpi",
@@ -9,30 +36,60 @@ const drawableFolder = [
 	"drawable-xxxhdpi",
 ];
 
-const zip =
-	"/Users/santiagotonarelli/Desktop/assets_Registration_addCard_2021-06-01.zip";
-const nameFile = "caca";
+const zip = argv.z || "";
+const nameFile = `${argv.f}.png` || "";
+const projectName = argv.p || "";
+
 let fileDefaultName;
+let targetPath;
 
-const targetPath = zip.substring(0, zip.lastIndexOf("/"));
+const unZip = () => {
+	try {
+		targetPath = zip.substring(0, zip.lastIndexOf("/"));
+		let readStream = fs.createReadStream(zip);
+		let extract = unzip.Extract({path: targetPath});
 
-let readStream = fs.createReadStream(
-	"/Users/santiagotonarelli/Desktop/assets_Registration_addCard_2021-06-01.zip"
-);
-let extract = unzip.Extract({path: targetPath});
-
-extract.on("error", () => console.error("Ja gil no funciona, copia a mano"));
-extract.on("close", () => {
-	console.log("Aguantaa...");
-    const fileDefaultNamePath = `${targetPath}/drawableFolder/`
-    fileDefaultName = str.substring(str.lastIndexOf('/')+1);
-	drawableFolder.forEach((d, i) => insert);
-});
-
-readStream.pipe(extract);
+		extract.on("error", () =>
+			console.error("Ja gil no funciona, copia a mano")
+		);
+		extract.on("close", () => {
+			console.log("Aguantaa...");
+			const fileDefaultNamePath = `${targetPath}/${drawableFolder[0]}/`;
+			try {
+				var files = fs.readdirSync(fileDefaultNamePath);
+				fileDefaultName = files[0];
+				drawableFolder.forEach(insert);
+				fs.unlinkSync(zip);
+				figlet("Listo,\n Anda a laburar!", function (err, data) {
+					if (err) {
+						console.log("Listo, Anda a laburar!");
+						return;
+					}
+					console.log(data);
+				});
+			} catch (error) {
+				console.error("Ja gil no funciona, copia a mano");
+				console.error(error);
+			}
+		});
+		readStream.pipe(extract);
+	} catch (error) {
+		console.error("Ja gil no funciona, copia a mano2");
+		console.error(error);
+	}
+};
 
 const insert = (d, i) => {
-    fs.rename(`${targetPath}/d/${};
+	fs.renameSync(
+		`${targetPath}/${drawableFolder[i]}/${fileDefaultName}`,
+		`${targetPath}/${drawableFolder[i]}/${nameFile}`
+	);
+	fs.copyFileSync(
+		`${targetPath}/${drawableFolder[i]}/${nameFile}`,
+		`${projectName}/levelUpWhiteLabelBuildApp/src/main/res/${drawableFolder[i]}/${nameFile}`
+	);
+	fs.unlinkSync(`${targetPath}/${drawableFolder[i]}/${nameFile}`);
+	fs.rmdirSync(`${targetPath}/${drawableFolder[i]}`);
+};
 
-
-}
+unZip();
